05f04394e81ba4016d9bb860078db573369577e0
Tribhuwan <mathincosmos@gmail.com>
2023-09-29 07:32:08 +0530

    created required routes folder

---
+++ app/register/page.tsx
@@ -0,0 +1,10 @@
+const page = () => {
+  return (
+    <form className="text-center">
+      <div className="className='flex flex-col justify-center h-[450px] w-[350px] mx-auto gap-2'">
+        
+      </div>
+    </form>
+  );
+};
+export default page;
